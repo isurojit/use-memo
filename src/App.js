e155a0235bd25390fea337e703bf56a0ff29532d
isurojit <iamsurojit@hotmail.com>
2022-03-11 12:07:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
     <div>
       <input type="number" value={number} onChange={inputHandler} />
       <button onClick={themeChangeHandler}>Change theme</button>
-      <div style={themeStyle}>{doubleNumber}</div>
+      <h1 style={themeStyle}>{doubleNumber}</h1>
     </div>
   );
 };
