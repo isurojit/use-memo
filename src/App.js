a7a0aec9352b251ee95876a156b137b2e20327ef
isurojit <iamsurojit@hotmail.com>
2022-03-11 11:54:13 +0530

    added methods

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <React.Fragment></React.Fragment>;
-}
+const App = () => {
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = slowFucntion(number);
 
+  const themeStyle = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+
+  const inputHandler = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const themeChangeHandler = (e) => {
+    setDark((pre) => {
+      console.log(!pre);
+      return !pre;
+    });
+  };
+
+  return (
+    <div>
+      <input type="number" value={number} onChange={inputHandler} />
+      <button onClick={themeChangeHandler}>Change theme</button>
+      <div style={themeStyle}>{doubleNumber}</div>
+    </div>
+  );
+};
+
+const slowFucntion = (number) => {
+  for (let i = 0; i <= 999999999; i++) {}
+  return number * 2;
+};
 export default App;
